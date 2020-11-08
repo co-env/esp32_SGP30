1261d5a3a3b8964aba69d02ed1c3e105b4c48c86
Renato Freitas <freitas-renato@outlook.com>
2020-11-07 21:22:15 -0300

    :zap: Change main device structure
    - Sensor data and i2c driver function pointers on same struct
    - Update example with i2c driver r/w functions

--- example/main/main.c
+++ example/main/main.c
@@ -13,39 +13,160 @@
 
 #include "SGP30.h"
 
+
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
+
+#define I2C_MASTER_SCL_IO CONFIG_I2C_MASTER_SCL               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO CONFIG_I2C_MASTER_SDA               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(CONFIG_I2C_MASTER_PORT_NUM) /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ CONFIG_I2C_MASTER_FREQUENCY        /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+
+i2c_port_t i2c_num = I2C_MASTER_NUM;
+
+
 static const char *TAG = "sgp30-test";
+
+
+
+esp_err_t i2c_master_driver_initialize(void) {
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+
+    conf.mode = I2C_MODE_MASTER;
+    conf.sda_io_num = I2C_MASTER_SDA_IO;
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.scl_io_num = I2C_MASTER_SCL_IO;
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE;
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ;
+    
+    i2c_param_config(i2c_master_port, &conf);
+    return i2c_driver_install(I2C_NUM_0, I2C_MODE_MASTER, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
+
+/**
+ * @brief generic function for reading I2C data
+ * 
+ * @param reg_addr register adress to read from 
+ * @param reg_data pointer to save the data read 
+ * @param len length of data to be read
+ * @param intf_ptr 
+ * 
+ * >init: dev->intf_ptr = &dev_addr;
+ * 
+ * @return ESP_OK if reading was successful
+ */
+int8_t main_i2c_read(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr) { // *intf_ptr = dev->intf_ptr
+    int8_t ret = 0; /* Return 0 for Success, non-zero for failure */
+
+    if (len == 0) {
+        return ESP_OK;
+    }
+
+    uint8_t chip_addr = *(uint8_t*)intf_ptr;
+    
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+
+    i2c_master_start(cmd);
+    
+    if (reg_addr != 0xff) {
+        i2c_master_write_byte(cmd, (chip_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+        i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+        i2c_master_start(cmd);
+    }
+    
+    i2c_master_write_byte(cmd, (chip_addr << 1) | I2C_MASTER_READ, ACK_CHECK_EN);
+
+    if (len > 1) {
+        i2c_master_read(cmd, reg_data, len - 1, ACK_VAL);
+    }
+    i2c_master_read_byte(cmd, reg_data + len - 1, NACK_VAL);
+    i2c_master_stop(cmd);
+
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    
+    i2c_cmd_link_delete(cmd);
+    
+    return ret;
+}
+
+
+/**
+ * @brief generic function for writing data via I2C 
+ *  
+ * @param reg_addr register adress to write to 
+ * @param reg_data register data to be written 
+ * @param len length of data to be written
+ * @param intf_ptr 
+ * 
+ * @return ESP_OK if writing was successful
+ */
+int8_t main_i2c_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr) {
+    int8_t ret = 0; /* Return 0 for Success, non-zero for failure */
+
+    uint8_t chip_addr = *(uint8_t*)intf_ptr;
+
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (chip_addr << 1) | I2C_MASTER_WRITE, ACK_CHECK_EN);
+    
+    if (reg_addr != 0xFF) {
+        i2c_master_write_byte(cmd, reg_addr, ACK_CHECK_EN);
+    }
+
+    i2c_master_write(cmd, reg_data, len, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+
+    ret = i2c_master_cmd_begin(i2c_num, cmd, 1000 / portTICK_RATE_MS);
+    
+    i2c_cmd_link_delete(cmd);
+    
+    return ret;
+}
+
 
 static void main_task(void *arg) {
     ESP_LOGI(TAG, "SGP30 main task initializing...");
 
-    sgp30_t main_sensor;
+    sgp30_dev_t main_sgp30_sensor;
 
-    sgp30_init(&main_sensor);
+    i2c_master_driver_initialize();
+
+    sgp30_init(&main_sgp30_sensor, (sgp30_read_fptr_t)main_i2c_read, (sgp30_write_fptr_t)main_i2c_write);
 
     // SGP30 needs to be read every 1s and sends TVOC = 400 14 times when initializing
     for (int i = 0; i < 14; i++) {
         vTaskDelay(1000 / portTICK_RATE_MS);
-        sgp30_IAQ_measure(&main_sensor);
-        ESP_LOGI(TAG, "SGP30 Calibrating... TVOC: %d,  eCO2: %d",  main_sensor.TVOC, main_sensor.eCO2);
+        sgp30_IAQ_measure(&main_sgp30_sensor);
+        ESP_LOGI(TAG, "SGP30 Calibrating... TVOC: %d,  eCO2: %d",  main_sgp30_sensor.TVOC, main_sgp30_sensor.eCO2);
     }
 
     // Read initial baselines 
     uint16_t eco2_baseline, tvoc_baseline;
-    sgp30_get_IAQ_baseline(&main_sensor, &eco2_baseline, &tvoc_baseline);
+    sgp30_get_IAQ_baseline(&main_sgp30_sensor, &eco2_baseline, &tvoc_baseline);
     ESP_LOGI(TAG, "BASELINES - TVOC: %d,  eCO2: %d",  tvoc_baseline, eco2_baseline);
 
 
     ESP_LOGI(TAG, "SGP30 main task is running...");
     while(1) {
         vTaskDelay(1000 / portTICK_RATE_MS);
-        sgp30_IAQ_measure(&main_sensor);
+        sgp30_IAQ_measure(&main_sgp30_sensor);
 
-        ESP_LOGI(TAG, "TVOC: %d,  eCO2: %d",  main_sensor.TVOC, main_sensor.eCO2);
+        ESP_LOGI(TAG, "TVOC: %d,  eCO2: %d",  main_sgp30_sensor.TVOC, main_sgp30_sensor.eCO2);
     }
 }
 
-void app_main(void)
-{
+void app_main(void) {
     // esp_log_level_set("*", ESP_LOG_VERBOSE);
     esp_log_level_set("SGP30-LIB", ESP_LOG_VERBOSE);
     
--- include/SGP30.h
+++ include/SGP30.h
@@ -1,7 +1,8 @@
 /**
  * @file SGP30.h
  *
- * @author Renato Freitas 
+ * @author Renato Freitas
+ * @author Isabella Bologna
  * 
  * @brief SGP30 library, based on Adafruit's one. 
  * ----> https://www.adafruit.com/product/3709
@@ -35,75 +36,75 @@
 #define NULL_REG 0xFF //????? 
 //!!!
 
+
+/*** I2C Driver Function Pointers ***/
+typedef int8_t (*sgp30_read_fptr_t)(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr);
+
+typedef int8_t (*sgp30_write_fptr_t)(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr);
+
 /** SGP30 Main Data Struct */
-typedef struct {
-    /**< The 48-bit serial number, this value is set when you call {@link begin()} **/
+typedef struct sgp30_dev {
+    /**< The 48-bit serial number, this value is set when you call sgp30_init */
     uint16_t serial_number[3];
 
     /**< The last measurement of the IAQ-calculated Total Volatile Organic
-            Compounds in ppb. This value is set when you call {@link IAQmeasure()} **/
+            Compounds in ppb. This value is set when you call IAQmeasure() **/
     uint16_t TVOC;
      
     /**< The last measurement of the IAQ-calculated equivalent CO2 in ppm. This
-            value is set when you call {@link IAQmeasure()} **/
+            value is set when you call IAQmeasure() */
     uint16_t eCO2;
 
     /**< The last measurement of the IAQ-calculated equivalent CO2 in ppm. This
-            value is set when you call {@link IAQmeasureRaw()} **/
+            value is set when you call IAQmeasureRaw() */
     uint16_t raw_H2;
 
     /**< The last measurement of the IAQ-calculated equivalent CO2 in ppm. This 
-            value is set when you call {@link IAQmeasureRaw()} **/
+            value is set when you call IAQmeasureRaw */
     uint16_t raw_ethanol;
 
-} sgp30_t;
-
-/*** Function Pointers ***/
-typedef int8_t (*sgp30_read_fptr_t)(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr);
-typedef int8_t (*sgp30_write_fptr_t)(uint8_t reg_addr, const uint8_t *reg_data, uint32_t len, void *intf_ptr);
-struct SGP30_dev {
-    /*< Interface function pointer used to enable the device address for I2C and chip selection for SPI */
+    /**< Interface pointer, used to store I2C address of the device */
     void *intf_ptr;
 
-    /*< Read function pointer */
+    /**< I2C read driver function pointer */
     sgp30_read_fptr_t i2c_read;
 
-    /*< Write function pointer */
+    /**< I2C write driver function pointer */
     sgp30_write_fptr_t i2c_write;
-};
+} sgp30_dev_t;
 
 
 /**
  *  @brief  Setups the hardware and detects a valid SGP30. Initializes I2C
  *          then reads the serialnumber and checks that we are talking to an SGP30.
  */
-void sgp30_init(sgp30_t *sensor, sgp30_read_fptr_t user_i2c_read, sgp30_write_fptr_t user_i2c_write);
+void sgp30_init(sgp30_dev_t *sensor, sgp30_read_fptr_t user_i2c_read, sgp30_write_fptr_t user_i2c_write);
 
 /**
  *   @brief  Commands the sensor to perform a soft reset using the "General
  *           Call" mode. Take note that this is not sensor specific and all devices that
  *           support the General Call mode on the on the same I2C bus will perform this.
  */
-void sgp30_softreset(sgp30_t *sensor);
+void sgp30_softreset(sgp30_dev_t *sensor);
 
 /**
  *   @brief  Commands the sensor to begin the IAQ algorithm. Must be called
  *           after startup.
  */
-void sgp30_IAQ_init(sgp30_t *sensor);
+void sgp30_IAQ_init(sgp30_dev_t *sensor);
 
 /**
  *  @brief  Commands the sensor to take a single eCO2/VOC measurement. Places
  *          results in sensor.TVOC and sensor.eCO2
  */
 
-void sgp30_IAQ_measure(sgp30_t *sensor);
+void sgp30_IAQ_measure(sgp30_dev_t *sensor);
 
 /**
  *  @brief  Commands the sensor to take a single H2/ethanol raw measurement.
  *          Places results in sensor.raw_H2 and sensor.raw_ethanol
  */
-void sgp30_IAQ_measure_raw(sgp30_t *sensor);
+void sgp30_IAQ_measure_raw(sgp30_dev_t *sensor);
 
 /*!
  *   @brief  Request baseline calibration values for both CO2 and TVOC IAQ
@@ -114,7 +115,7 @@ void sgp30_IAQ_measure_raw(sgp30_t *sensor);
  *   @param  tvoc_base
  *           A pointer to a uint16_t which we will save the calibration value to
  */
-void sgp30_get_IAQ_baseline(sgp30_t *sensor, uint16_t *eco2_base, uint16_t *tvoc_base);
+void sgp30_get_IAQ_baseline(sgp30_dev_t *sensor, uint16_t *eco2_base, uint16_t *tvoc_base);
 
 /**
  *  @brief  Assign baseline calibration values for both CO2 and TVOC IAQ
@@ -124,7 +125,7 @@ void sgp30_get_IAQ_baseline(sgp30_t *sensor, uint16_t *eco2_base, uint16_t *tvoc
  *  @param  tvoc_base
  *          A uint16_t which we will save the calibration value from
  */
-void sgp30_set_IAQ_baseline(sgp30_t *sensor, uint16_t eco2_base, uint16_t tvoc_base);
+void sgp30_set_IAQ_baseline(sgp30_dev_t *sensor, uint16_t eco2_base, uint16_t tvoc_base);
 
 /**
  *  @brief  Set the absolute humidity value [mg/m^3] for compensation to
@@ -134,44 +135,7 @@ void sgp30_set_IAQ_baseline(sgp30_t *sensor, uint16_t eco2_base, uint16_t tvoc_b
  *          If the absolute humidity is set to zero, humidity compensation
  *          will be disabled.
  */
-void sgp30_set_humidity(sgp30_t *sensor, uint32_t absolute_humidity);
-
-
-
-/*********************************************************************************************
- * Measurement routine: START condition, the I2C WRITE header (7-bit I2C device address plus 0
- * as the write bit) and a 16-bit measurement command.
- *
- * All commands are listed in TABLE 10 on the datasheet. 
- * 
- * 
- * After the sensor has completed the measurement, the master can read the measurement results by sending a START
- * condition followed by an I2C READ header. The sensor will respond with the data.
- * 
- *! Each byte must be acknowledged by the microcontroller with an ACK condition for the sensor to continue sending data.
- * If the sensor does not receive an ACK from the master after any byte of data, it will not continue sending data.
- **********************************************************************************************/
-
-/**
- *  @brief Executes commands based on SGP30 Command Table 
- * 
- *  @see https://www.sensirion.com/fileadmin/user_upload/customers/sensirion/Dokumente/9_Gas_Sensors/Datasheets/Sensirion_Gas_Sensors_SGP30_Datasheet.pdf
- *       Table #10
- */
-
-bool sgp30_execute_command(uint8_t command[], uint8_t command_len, uint16_t delay,
-                           uint16_t *read_data, uint8_t read_len);
-
-/**
- * @brief Data and commands are protected with a CRC checksum to increase the communication reliability.
- * The 16-bit commands that are sent to the sensor already include a 3-bit CRC checksum.
- * Data sent from and received by the sensor is always succeeded by an 8-bit CRC.
- *! In write direction it is mandatory to transmit the checksum, since the SGP30 only accepts data if it is followed by the correct
- *! checksum. 
- *
- ** In read direction it is up to the master to decide if it wants to read and process the checksum
- */
-uint8_t sgp30_calculate_CRC(uint8_t *data, uint8_t len);
+void sgp30_set_humidity(sgp30_dev_t *sensor, uint32_t absolute_humidity);
 
 
 #endif  // __SGP30_H__
